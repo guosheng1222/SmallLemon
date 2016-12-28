bb6cdf66bdc5be8d73eff9c115dd6234e56e063d
guosheng1222 <347119219@qq.com>
2016-12-28 19:54:34 +0800

    by 赵国胜 2016/12/28 19:54

--- app/src/main/java/com/example/smalllemon/MainActivity.java
+++ app/src/main/java/com/example/smalllemon/MainActivity.java
@@ -1,59 +1,46 @@
 package com.example.smalllemon;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import com.example.base.BaseActivity;
+import com.example.fragment.Fragment1;
+import com.example.fragment.Fragment2;
+import com.example.fragment.Fragment3;
+import com.example.fragment.Fragment4;
+
+import java.util.ArrayList;
 
 public class MainActivity extends BaseActivity {
 
     private ViewPager vp;
-    int[] s=new int[]{R.mipmap.aa,R.mipmap.bb,R.mipmap.cc,R.mipmap.dd,R.mipmap.ee,R.mipmap.ff};
+    private ArrayList<Fragment> list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
-    }
-
-    private void initView() {
         vp = (ViewPager) findViewById(R.id.main_vp);
-        vp.setAdapter(new PagerAdapter() {
+        Fragment f1=new Fragment1();
+        Fragment f2=new Fragment2();
+        Fragment f3=new Fragment3();
+        Fragment f4=new Fragment4();
+        list = new ArrayList<>();
+        list.add(f1);
+        list.add(f2);
+        list.add(f3);
+        list.add(f4);
+        vp.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return list.get(position);
+            }
+
             @Override
             public int getCount() {
-                return s.length;
-            }
-
-            @Override
-            public boolean isViewFromObject(View view, Object object) {
-                return view==object;
-            }
-
-            @Override
-            public Object instantiateItem(ViewGroup container, int position) {
-                ImageView image=new ImageView(MainActivity.this);
-                image.setImageResource(s[position]);
-                image.setScaleType(ImageView.ScaleType.FIT_XY);
-                image.setFitsSystemWindows(true);
-                container.addView(image);
-                image.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent in=new Intent(MainActivity.this,LunchActivity.class);
-                        startActivity(in);
-                    }
-                });
-                return image;
-            }
-
-            @Override
-            public void destroyItem(ViewGroup container, int position, Object object) {
-                container.removeView((View) object);
+                return list.size();
             }
         });
     }
