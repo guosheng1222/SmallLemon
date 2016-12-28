3f54aa9e78b2423780ca9cb0f854da306a2971dc
guosheng1222 <347119219@qq.com>
2016-12-28 15:04:38 +0800

    by 赵国胜 2016/12/28 15:00

--- app/src/main/java/com/example/smalllemon/MainActivity.java
+++ app/src/main/java/com/example/smalllemon/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.smalllemon;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initSystemBar(this);
         setContentView(R.layout.activity_main);
+
     }
 }
