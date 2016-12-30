387821fedfd76edf5efc44083a7c56e6f8b946e1
hasee <1204304747@qq.com>
2016-12-30 15:39:36 +0800

    登录界面-zhp

--- app/src/main/java/com/example/smalllemon/MainActivity.java
+++ app/src/main/java/com/example/smalllemon/MainActivity.java
@@ -139,4 +139,20 @@ public class MainActivity extends BaseActivity {
 
     }
 
+    private long mPressedTime = 0;
+
+    /**
+     * 双击退出应用
+     */
+    @Override
+    public void onBackPressed() {
+        long mNowTime = System.currentTimeMillis();//获取第一次按键时间
+        if ((mNowTime - mPressedTime) > 2000) {//比较两次按键时间差
+            Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+            mPressedTime = mNowTime;
+        } else {//退出程序
+            this.finish();
+        }
+    }
+
 }
