2819026253102ff72c3a9d8bf91384598a4aec46
guosheng1222 <347119219@qq.com>
2016-12-31 15:39:07 +0800

    by 赵国胜 2016/12/31 15:38 没啥操作哦

--- app/src/main/java/com/example/utils/UrlUtils.java
+++ app/src/main/java/com/example/utils/UrlUtils.java
@@ -6,5 +6,77 @@ package com.example.utils;
  */
 
 public class UrlUtils {
+    /**
+     * 主页面接口
+     */
+    //首页轮播图
+    public static final String main_viewager="http://www.yulin520.com/a2a/broadcast/files?sign=7442C54B6DAFB81CEB01588164F3CCA8&ts=1482907765&pageSize=9&page=1";
+    //助攻节日
+    public static final String holiday1="http://www.yulin520.com/a2a/festival/next?sign=5D6B618EA7D05C14090FCCF82C566526&ts=1482907765";
+    //点击助攻节日
+    public static final String clickholiday="http://www.yulin520.com/a2a/h/i/app/next_festival";
+    //恋爱期 恋乎社区
+    public static final String loving="http://www.yulin520.com/a2a/forum/recommend/withIndexImg?sign=CFB612BCCC0E98F08157E01017F8AA49&pageSize=12&emotionStage=2&ts=1482908782&page=1";
+    //单身期 恋乎社区
+    public static final String single="http://www.yulin520.com/a2a/forum/recommend/withIndexImgsign=2ED307930C123871704A9C84C46CFC5F&pageSize=12&emotionStage=1&ts=1482907765&page=1";
+    //冷暖共知
+    public static final String cold="http://www.yulin520.com/a2a/news/dissertation?sign=C49E4703030EB2FF11E0E7377FB88709&pageSize=1&queryData=&ts=1482907765&page=1";
+    //恋爱氧气
+    public static final String lovingCO2="http://www.yulin520.com/a2a/news/sd/list?sign=816863C6854591D9E607BE273A05CE65&ts=1482907765&pageSize=6&page=1";
+    //点击查看更多
+    public static final String lookMore="http://www.yulin520.com/a2a/news/sd/list?sign=4508DEDF231D4E571E987BBE7C6CAB28&ts=1145666944&pageSize=10&page=1";
+
+    /**
+     * 社区接口
+     */
+
+    //精选
+    public static final String selected="http://www.yulin520.com/a2a/forum/allTypeList?sign=1ED39AA49B6594114FB04896D4716775&pageSize=10&sort=2&ts=1482920354&page=1&forumType=0";
+
+    //全部
+
+    public static final String All="http://www.yulin520.com/a2a/forum/allTypeList?sign=0A1CA7FA70FD4F4B1E141438594A4C10&pageSize=10&sort=2&ts=1482920553&page=1&forumType=0";
+
+
+    /**
+     * 你的月亮我的心
+     */
+    // 置顶
+    public static final String moonTop="http://www.yulin520.com/a2a/forum/typeTop?sign=565F990AE10D44353542163CF14F024F&pageSize=10&sort=1&ts=1482920713&page=1&forumType=10";
+    //内容
+    public static final String moonContent="http://www.yulin520.com/a2a/forum/typeList?sign=565F990AE10D44353542163CF14F024F&pageSize=10&sort=1&ts=1482920713&page=1&forumType=10";
+
+    /**
+     * 恋爱羞羞事
+     */
+        //置顶
+    public static final String shyTop="http://www.yulin520.com/a2a/forum/typeTop?sign=1848F133C16B5BAFF96E04D2509D45F7&pageSize=10&sort=1&ts=1482921038&page=1&forumType=11";
+    //内容
+    public static final String shyContent="http://www.yulin520.com/a2a/forum/typeList?sign=1848F133C16B5BAFF96E04D2509D45F7&pageSize=10&sort=1&ts=1482921038&page=1&forumType=11";
+
+    /**
+     * 约会必杀技
+     */
+        //置顶
+        public static final String killTop="";
+        //内容
+        public static final String killContent="";
+
+    /**
+     * 主要看颜值
+     */
+
+    //置顶
+    public static final String faceTop="http://www.yulin520.com/a2a/forum/typeTop?sign=65F663FA05628041064BEAED02C320E1&pageSize=10&sort=1&ts=1482921283&page=1&forumType=13";
+    //内容
+    public static final String faceContent="http://www.yulin520.com/a2a/forum/typeList?sign=65F663FA05628041064BEAED02C320E1&pageSize=10&sort=1&ts=1482921283&page=1&forumType=13";
+
+    /**
+     * 恋爱直播间
+     */
+    //置顶
+    public static final String showTop="http://www.yulin520.com/a2a/forum/typeTop?sign=F7F111B1646C746D12A5120396D2A952&pageSize=10&sort=1&ts=1482921362&page=1&forumType=14";
+    //内容
+    public static final String showContent="http://www.yulin520.com/a2a/forum/typeList?sign=F7F111B1646C746D12A5120396D2A952&pageSize=10&sort=1&ts=1482921362&page=1&forumType=14";
 
 }
